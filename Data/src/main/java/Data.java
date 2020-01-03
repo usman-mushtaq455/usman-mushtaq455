b1610cf808d39e2322d039057b007e1a702d507b
usman <usman@192.168.0.122>
2020-01-03 13:24:34 +0500

    Pushed

---
+++ Data/src/main/java/Data.java
@@ -0,0 +1,13 @@
+
+public class Data {
+	/**
+	 * This is the <b>simple</b> integer calculator app 
+	 * @author Usman Mushtaq
+	 * @since 03/12/2019
+	 * @deprecated Jdk 13 is available
+	 * @version 1.0
+	 */
+public void abc() {
+	
+}
+}
---
+++ Data/src/main/java/DataTest.java
@@ -0,0 +1,9 @@
+import org.junit.Test;
+
+public class DataTest {
+@Test
+public void test1() {
+	Data dt=new Data();
+	dt.abc();
+}
+}
